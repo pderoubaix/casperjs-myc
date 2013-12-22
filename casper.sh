d9cb929f3ef2c9f2d679f36bed770b9fbbe413da
pderoubaix <cendres@gmail.com>
2013-12-22 23:20:38 +0100

    add script for jenkins integration

--- casper.sh
+++ casper.sh
@@ -1,5 +1,6 @@
 export PATH=/home/vagrant/local/bin:/home/vagrant/local/bin:/home/vagrant/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/opt/vagrant_ruby/
-rm -f /tmp/casper-*
+rm -f /tmp/casper-*.xml
 cd /home/nodedata/mycasperttest/
+rm -f screenshots/*
 casperjs test tests/myEpgAtg.js --username=$1 --password=$2 --force --xunit=/tmp/casper-$(date +"%Y.%m.%d.%S.%N").xml
 casperjs test tests/myProspect.js --username=$1 --password=$2 --force --xunit=/tmp/casper-$(date +"%Y.%m.%d.%S.%N").xml
